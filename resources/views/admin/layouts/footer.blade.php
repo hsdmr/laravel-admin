04dcf889210ee69a8c8bda6a9d820cc53c04b059
hsdmr <hsdmrsoft@gmail.com>
2020-12-03 23:25:24 +0300

    devam

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -3,7 +3,7 @@
     <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      Anything you want
+      <!--Anything you want-->
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2020 <a href="https://hsdmrsoft.com">HsdmrSOFT</a>.</strong> Tüm Hakları Saklıdır.
