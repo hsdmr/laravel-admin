3517d8293adbf385725f8c8beeb8cfb39bc18b79
hsdmr <hsdmrsoft@gmail.com>
2020-12-13 19:26:03 +0300

    user table

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -71,49 +71,5 @@
 @endsection
 
 @section('script')
-<script>
-    $(function(){
-        $('#table1').DataTable({
-            "paging": true,
-            "lengthChange": true,
-            "searching": true,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "responsive": true,
-            "language": {
-                    "sDecimal":        ",",
-                    "sEmptyTable":     "Tabloda herhangi bir veri mevcut değil",
-                    "sInfo":           "_TOTAL_ kayıttan _START_ - _END_ arasındaki kayıtlar gösteriliyor",
-                    "sInfoEmpty":      "Kayıt yok",
-                    "sInfoFiltered":   "(_MAX_ kayıt içerisinden bulunan)",
-                    "sInfoPostFix":    "",
-                    "sInfoThousands":  ".",
-                    "sLengthMenu":     "Sayfada _MENU_ kayıt göster",
-                    "sLoadingRecords": "Yükleniyor...",
-                    "sProcessing":     "İşleniyor...",
-                    "sSearch":         "Ara:",
-                    "sZeroRecords":    "Eşleşen kayıt bulunamadı",
-                    "oPaginate": {
-                        "sFirst":    "İlk",
-                        "sLast":     "Son",
-                        "sNext":     "Sonraki",
-                        "sPrevious": "Önceki"
-                    },
-                    "oAria": {
-                        "sSortAscending":  ": artan sütun sıralamasını aktifleştir",
-                        "sSortDescending": ": azalan sütun sıralamasını aktifleştir"
-                    },
-                    "select": {
-                        "rows": {
-                            "_": "%d kayıt seçildi",
-                            "0": "",
-                            "1": "1 kayıt seçildi"
-                        }
-                    }
-                }
-        });
-    });
 
-</script>
 @endsection
