420e23ee6b9756a30099e3289139260f35589f33
hsdmr <hsdmrsoft@gmail.com>
2021-06-28 17:05:07 +0300

    fix

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -19,7 +19,7 @@
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
-        <h1 class="text-danger text-center mt-5">{{ __('main.Welcome') }}</h1>
+        <center class="p-4 m-5 alert-success" style="font-size: 1.5rem">Thanks for coming this far. I hope it helps. I'll take your one star if you like it :)</center>
       </div>
     </div>
   </div>
